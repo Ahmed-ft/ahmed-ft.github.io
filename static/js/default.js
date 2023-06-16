b49dc584dd3c50c481e525cd22e9db2849198373
Ahmed-ft- <ahmedftita2015@gmail.com>
2023-06-16 16:19:28 +0200

    no comment

---
+++ static/js/default.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    
+    $(".sec1").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".s1").offset().top},
+            'slow');
+    });
+    $(".sec2").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".s2").offset().top},
+            'slow');
+    });
+    $(".sec3").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".s3").offset().top},
+            'slow');
+    });
+    $(".sec4").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".s4").offset().top},
+            'slow');
+    });
+    $(".sec5").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".s5").offset().top},
+            'slow');
+    });
+    $(".sec7").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".s7").offset().top},
+            'slow');
+    });
+
+});
