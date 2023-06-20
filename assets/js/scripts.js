9adf2ae93542ffe8d10024c629b06ee740019779
Ahmed-ft- <ahmedftita2015@gmail.com>
2023-06-20 11:38:32 +0200

    no comment

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    
+
+
+    $('a#getProfile').attr({target: '_blank', 
+                    href  : '/assets/files/COMPANY_CV.pdf'});
+
+    $('a#getHse').attr({target: '_blank', 
+                    href  : '/assets/files/HSE_MANUAL.pdf'});
+
+
+});
