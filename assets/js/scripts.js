ccaa45e56ea21613bd3d2f91a4471cafddf705dc
Ahmed-ft- <ahmedftita2015@gmail.com>
2023-06-20 15:05:27 +0200

    no comment

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,6 +7,19 @@ $(document).ready(function () {
 
     $('a#getHse').attr({target: '_blank', 
                     href  : '/assets/files/HSE_MANUAL.pdf'});
+                    
+    $('a#transport-s').attr({target: '_blank', 
+                    href  : '/assets/files/TRANSPORT_PROFILE.pdf'});
 
+    $('.exit').click(function (e) { 
+        e.preventDefault();
+        $(this).parent().css('display', 'none')
+    });
+
+    $('.service-board-trigger').click(function (e) { 
+        e.preventDefault();
+        var id = $(this).attr('id');
+        $(`.${id}`).css('display', 'flex')
+    });
 
 });
